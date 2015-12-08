1d81749e715fff32d0cbc9b5af01c789d50fdbb8
MUHAMMAD HASSAN <hmuham7@vt.edu>
2015-12-08 12:47:46 -0500

    Added encryption algorithm and related comments in enc.py

--- enc.py
+++ enc.py
@@ -20,6 +20,24 @@ key = raw_input("Enter the key to encrypt the message: ")
 f_in = open(act_file, 'r+')
 f_out = open(enc_file, 'w')
 
+# for loop to read all the lines in text file and perform ecnryption
+for line in f_in:
+	string = line.rstrip()  # Making sure of excluding the newline character
+	newkeystring = ''
+	hexstring = string.encode("hex")  # converting the text into hexa-decimal format
+	keystring = key.decode("hex")
+	
+	# inner for loop to extend the key to upto the length to string (line size) as that key will be used late to XOR, so the length
+	# has to be a match
+	for i in range(0,len(string)):
+    		newkeystring+=keystring[i%len(keystring)]
+
+	newkey = newkeystring.encode("hex")  # encoding the newly resized key to hexadecimal
+	cipherstring = int(hexstring, 16) ^ int(newkey, 16)  # performing XOR operation
+	cipher = '{:x}'.format(cipherstring)
+	cipher_str = cipher.decode("hex")
+	f_out.write(cipher_str)   # storing the XOR-ed string into a text file (or otherwise called encrypted message)
+
 
 # closing the files after performing operations
 f_in.close()
